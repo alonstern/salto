959b115f773f5680a6d5261ac034238ea54ef1e1
Alon Stern <alon.stern@salto.io>
2021-02-22 13:56:04 +0200

    .

--- packages/netsuite-adapter/src/client/suiteapp_client/suiteapp_client.ts
+++ packages/netsuite-adapter/src/client/suiteapp_client/suiteapp_client.ts
@@ -16,53 +16,47 @@
 import Bottleneck from 'bottleneck'
 import OAuth from 'oauth-1.0a'
 import crypto from 'crypto'
-import axios, { AxiosResponse } from 'axios'
+import axios from 'axios'
+import Ajv from 'ajv'
 import { logger } from '@salto-io/logging'
-import { Credentials } from './credentials'
+import _ from 'lodash'
+import { Credentials } from '../credentials'
+import { HttpMethod, isError, SavedSearchQuery, SavedSearchResults, SavedSearchSuccessResults,
+  SAVED_SEARCH_RESULTS_SCHEMA, SuiteAppClientParameters, SuiteQLResults, SUITE_QL_RESULTS_SCHEMA } from './types'
 
 
 const CONSUMER_KEY = '3db2f2ec0bd98c4eee526ea0b8da876d1d739597e50ee593c67c0f2c34294073'
 const CONSUMER_SECRET = '4c8399c03043f4ff2889610d260fc76037d126c840f83b3e6a4e6f4ddf3b0b79'
 const PAGE_SIZE = 1000
 
-type HttpMethod = 'POST' | 'GET'
-
 const log = logger(module)
-
-export type SuiteAppClientParameters = {
-  credentials: Credentials
-  callsLimiter: Bottleneck
-}
-
-export type SavedSearchQuery = {
-  type: string
-  columns: string[]
-  filters: string[][]
-}
 
 export class SuiteAppClient {
   private credentials: Credentials
   private callsLimiter: Bottleneck
   private suiteQLUrl: URL
   private savedSearchUrl: URL
+  private ajv: Ajv
 
   constructor(params: SuiteAppClientParameters) {
     this.credentials = params.credentials
     this.callsLimiter = params.callsLimiter
+    // TODO: change account id conversion
     this.suiteQLUrl = new URL(`https://${params.credentials.accountId}.suitetalk.api.netsuite.com/services/rest/query/v1/suiteql`)
     this.savedSearchUrl = new URL(`https://${params.credentials.accountId}.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=customscript_salto_search_restlet&deploy=customdeploy_salto_search_restlet`)
+    this.ajv = new Ajv({ allErrors: true, strict: false })
   }
 
   public async runSuiteQL(query: string):
-    Promise<Record<string, string>[] | undefined> {
+    Promise<Record<string, unknown>[] | undefined> {
     let hasMore = true
-    const items: Record<string, string>[] = []
+    const items: Record<string, unknown>[] = []
     for (let offset = 0; hasMore; offset += PAGE_SIZE) {
       try {
         // eslint-disable-next-line no-await-in-loop
-        const response = await this.sendSuiteQLRequest(query, offset, PAGE_SIZE)
-        items.push(...response.data.items)
-        hasMore = response.data.hasMore
+        const results = await this.sendSuiteQLRequest(query, offset, PAGE_SIZE)
+        items.push(...results.items.map(item => _.omit(item, ['links'])))
+        hasMore = results.hasMore
       } catch (error) {
         log.error('SuiteQL query error', { error })
         return undefined
@@ -72,19 +66,15 @@ export class SuiteAppClient {
   }
 
   public async runSavedSearchQuery(query: SavedSearchQuery):
-    Promise<Record<string, string>[] | undefined> {
+    Promise<Record<string, unknown>[] | undefined> {
     let hasMore = true
-    const items: Record<string, string>[] = []
-    for (let offset = 0; hasMore; offset += 1000) {
+    const items: Record<string, unknown>[] = []
+    for (let offset = 0; hasMore; offset += PAGE_SIZE) {
       try {
         // eslint-disable-next-line no-await-in-loop
-        const response = await this.sendSavedSearchRequest(query, offset, PAGE_SIZE)
-        if (response.data.status === 'error') {
-          log.error('Saved search query error', { responseData: response.data })
-          return undefined
-        }
-        items.push(...response.data.results)
-        hasMore = response.data.results.length === 1000
+        const results = await this.sendSavedSearchRequest(query, offset, PAGE_SIZE)
+        items.push(...results.results)
+        hasMore = results.results.length === PAGE_SIZE
       } catch (error) {
         log.error('Saved search query error', { error })
         return undefined
@@ -95,7 +85,7 @@ export class SuiteAppClient {
 
 
   private async sendSuiteQLRequest(query: string, offset: number, limit: number):
-  Promise<AxiosResponse> {
+  Promise<SuiteQLResults> {
     const url = new URL(this.suiteQLUrl.href)
     url.searchParams.append('limit', limit.toString())
     url.searchParams.append('offset', offset.toString())
@@ -104,16 +94,22 @@ export class SuiteAppClient {
       ...this.generateHeaders(url, 'POST'),
       prefer: 'transient',
     }
-    return this.callsLimiter.schedule(() => axios.post(
+    const response = await this.callsLimiter.schedule(() => axios.post(
       url.href,
       { q: query },
       { headers },
     ))
+
+    if (!this.ajv.validate<SuiteQLResults>(SUITE_QL_RESULTS_SCHEMA, response.data)) {
+      throw new Error(`Got invalid results from the SuiteQL query: ${this.ajv.errorsText()}`)
+    }
+
+    return response.data
   }
 
   private async sendSavedSearchRequest(query: SavedSearchQuery, offset: number, limit: number):
-  Promise<AxiosResponse> {
-    return this.callsLimiter.schedule(() => axios.post(
+  Promise<SavedSearchSuccessResults> {
+    const response = await this.callsLimiter.schedule(() => axios.post(
       this.savedSearchUrl.href,
       {
         ...query,
@@ -122,6 +118,16 @@ export class SuiteAppClient {
       },
       { headers: this.generateHeaders(this.savedSearchUrl, 'POST') },
     ))
+
+    if (!this.ajv.validate<SavedSearchResults>(SAVED_SEARCH_RESULTS_SCHEMA, response.data)) {
+      throw new Error(`Got invalid results from the saved search query: ${this.ajv.errorsText()}`)
+    }
+
+    if (isError(response.data)) {
+      throw new Error(`Saved search query failed. Message: ${response.data.message}, error ${response.data.error}`)
+    }
+
+    return response.data
   }
 
   private generateHeaders(url: URL, method: HttpMethod): Record<string, string> {
